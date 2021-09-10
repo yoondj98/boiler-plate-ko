0c2d06819ecd17865b84e762e1ad772d9d36941f
Xerath <yoondj98@naver.com>
2021-09-10 22:34:13 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+const config = require("./config/key")
+
+//applcation/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://Xerath:xerath0419!@boilerplate.0bnkh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=> console.log('MongoDB Connected'))
-  .catch(err => console.log(err))
+mongoose.connect(config.mongoURI).catch(err => console.log(err))
 
-  app.get('/', (req, res) => res.send('Hello 월드~'))
+  app.get('/', (req, res) => res.send('Hello 월드~ 새해 복 많이 받으세요 네?'))
+
+
+  app.post('/register', (req, res) => {
+      //회원가입할 때 필요한 정보들을 client에서 가져오면
+      //그것들을 데이터 베이스에 넣어준다.
+
+      const user = new User(req.body)
+
+      user.save((err, userInfo) => {
+          if(err) return res.json({ success: false, err})
+          return res.status(200).json({
+              success: true
+          })
+      })
+    })
 
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
